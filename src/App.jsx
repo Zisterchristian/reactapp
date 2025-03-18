fa7cc99b084b00137810424ca3e7944251d1b2bd
Zisterchristian <jaggling@gmail.com>
2025-03-18 22:09:39 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Heading from './components/Heading'
+import Card from './components/Card'
+
+export default function App() {
+  return (
+    <div>
+      <Heading text="First heading" />
+      <Heading text="Second heading" />
+      <Heading text="Third heading" />
+
+      <Card title="Product 1" />
+      
+
+
+
+
+    </div>
+  )
+}
